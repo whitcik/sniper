c692235f4b5790c7ce907a8a1527da0515293502
whitcik <whitcik@gmail.com>
2017-04-16 16:47:42 +0200

    Init repo with create react app - added sass support

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Starter Sniper</h2>
+        </div>
+        <p className="App-intro">
+          Start
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
